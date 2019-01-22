edfca0d002764ebdb5b6de1287857545f0ed883d
mkay0961 <mkay0961@gmail.com>
2019-01-22 12:01:34 -0500

    created database

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,26 @@
-require 'bundler'
+# require 'bundler'
+# Bundler.require
+#
+# ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+# require_all 'lib'
+
+require 'bundler/setup'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+
+DBNAME = "MLB_Database"
+
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+
+DBRegistry[ENV["ACTIVE_RECORD_ENV"]].connect!
+DB = ActiveRecord::Base.connection
+
+if ENV["ACTIVE_RECORD_ENV"] == "test"
+  ActiveRecord::Migration.verbose = false
+end
