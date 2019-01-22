42176817a4303bcb4eef2ac8185b2080259adc7d
mkay0961 <mkay0961@gmail.com>
2019-01-22 14:33:33 -0500

    Added more dummy data

--- db/migrate/001_create_players.rb
+++ db/migrate/001_create_players.rb
@@ -2,7 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[4.2]
   def change
     create_table :players do |t|
       t.string :full_name
-      t.string :postion
+      t.string :position
       t.integer :jersey_number
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Team.destroy_all
+Player.destroy_all
+PlayerTeam.destroy_all
+
 blues = Team.find_or_create_by(name:"washington blue" ,city: "washington", league: "NL", venue: "blue stadium" )
 reds = Team.find_or_create_by(name:"DC reds" ,city: "washington DC", league: "NL", venue: "reds stadium" )
 orange = Team.find_or_create_by(name:"DC oranges" ,city: "washington", league: "NL", venue: "orange stadium" )
@@ -7,4 +11,7 @@ fred = Player.find_or_create_by(full_name: "fred smith", position: "catcher", je
 sarah = Player.find_or_create_by(full_name: "sarah smith", position: "short stop", jersey_number: 60)
 matt = Player.find_or_create_by(full_name: "matt matt", position: "pitcher", jersey_number: 69)
 
-#join1 = PlayerTeam.find_or_create_by(player_id: alex, team_id: blues)
+join1 = PlayerTeam.find_or_create_by(player: alex, team: blues)
+join2 = PlayerTeam.find_or_create_by(player: fred, team: reds)
+join3 = PlayerTeam.find_or_create_by(player: sarah, team: blues)
+join4 = PlayerTeam.find_or_create_by(player: matt, team: blues)
