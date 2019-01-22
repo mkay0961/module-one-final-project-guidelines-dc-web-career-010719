2434eb613f3c0804f51086a70713572fc7af40f6
James Clement <jameseclement@gmail.com>
2019-01-22 15:14:31 -0500

    changed environment to match tv land lab

--- Gemfile
+++ Gemfile
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
-
+gem 'pry'
+gem "rspec"
 gem "sinatra-activerecord"
 gem "sqlite3"
-gem "pry"
-gem "require_all"
-gem "rspec"
-gem "activerecord"
 gem "rake"
+gem "database_cleaner"
--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,19 @@
 require_relative '../config/environment'
 
 
-
-puts "Matt was here"
-puts "james was here"
+puts "Welcome to the Baseball Database      _--_     dMb
+                                         __(._  )   d0P
+                                           <  (D)  .MP
+                                         .~ \ /~```M-.
+                                       .~    V    Mo_ \
+-------============((((}{)             (   (___. {:)-./
+                                        ~._____.(:}
+                        '94 the wolfe    /     .M\
+                                        /      "" \
+                                        |    /\   |
+                                        /   /  \   \
+                                       /   /    \   \
+                                       \__/      \__/
+                                       / /        | |
+                                      .^V^.      .^V^.
+                                       +-+        +-+"
