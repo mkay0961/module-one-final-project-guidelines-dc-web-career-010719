39fd896083a41c3ff2df37a3d2cc70fbcde5ce17
James Clement <jameseclement@gmail.com>
2019-01-22 19:33:10 -0500

    enabled search for player

--- app/UI/player_menu.rb
+++ app/UI/player_menu.rb
@@ -1,5 +1,23 @@
 def player_menu
 puts "Which player would you like to know about?"
- Player.all.each_with_index{|player, index| puts "#{index+1}. #{player.full_name}"}
+input = gets.chomp
+ # Player.all.each_with_index{|player, index| puts "#{index+1}. #{player.full_name}"}
+results = Player.all.select{|player| player.full_name.include?(input)}
+
+  if results.length > 0
+   x = results.each_with_index.map{|player, index|  "#{index+1}. #{player.full_name}"}
+   puts x
+  else
+   puts "no players found"
+  end
+
+
+ puts "Select your player by number"
+ num = gets.chomp
+ selected = x[num.to_i-1]
+ puts selected
+
+
+
 
 end
