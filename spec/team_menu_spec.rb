f2db2d7c4181ae1f62cc1a35ac49e9c89afffc5c
mkay0961 <mkay0961@gmail.com>
2019-01-24 12:58:06 -0500

    hidden respec

---
+++ spec/team_menu_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../config/environment'
+
+describe "TeamMenu" do
+
+
+  it "return the city, venue and state for the team 'Washington Nationals'" do
+    #binding.pry
+        expect{get_city_venue_state(Team.find(570))}.to output("The Washington Nationals are from Washington, DC.\nThey play in Nationals Park.\n").to_stdout
+  end
+
+
+  it "return the league for the team 'Washington Nationals'" do
+    #binding.pry
+        expect{get_league(Team.find(570))}.to output("The Washington Nationals are in the National League.\n").to_stdout
+  end
+
+  it "return the division for the team 'Washington Nationals'" do
+    #binding.pry
+        expect{get_division(Team.find(570))}.to output("The Washington Nationals are in the National League East division.\n").to_stdout
+  end
+
+  it "return the phone number for the team 'Washington Nationals'" do
+    #binding.pry
+        expect{get_phonenumber(Team.find(570))}.to output("The phone number for the Washington Nationals is (202) 349-0400.\n").to_stdout
+  end
+
+
+end
