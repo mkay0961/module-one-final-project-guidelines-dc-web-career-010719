b528036099ea50b6b9a7832c77ce0b0594ca5ab4
James Clement <jameseclement@gmail.com>
2019-01-24 13:03:37 -0500

    changed find by name

--- spec/player_menu_spec.rb
+++ spec/player_menu_spec.rb
@@ -6,35 +6,35 @@ describe "PlayerMenu" do
 
       it "return the twitter id for the player 'alec asher'" do
         #binding.pry
-            expect{get_twitter(Player.find(14988))}.to output("Alec Asher is on twitter at  @TheBigCat28.\n").to_stdout
+            expect{get_twitter(Player.find_by(full_name:"Alec Asher"))}.to output("Alec Asher is on twitter at  @TheBigCat28.\n").to_stdout
       end
       it "return the nickname for the player 'alec asher'" do
         #binding.pry
-            expect{get_nickname(Player.find(14988))}.to output("Alec Asher also goes by The Big Cat.\n").to_stdout
+            expect{get_nickname(Player.find_by(full_name:"Alec Asher"))}.to output("Alec Asher also goes by The Big Cat.\n").to_stdout
       end
       it "return the throws for the player 'alec asher'" do
         #binding.pry
-            expect{get_throws(Player.find(14988))}.to output("Alec Asher throws R.\n").to_stdout
+            expect{get_throws(Player.find_by(full_name:"Alec Asher"))}.to output("Alec Asher throws R.\n").to_stdout
       end
       it "return the bats for the player 'alec asher'" do
         #binding.pry
-            expect{get_bats(Player.find(14988))}.to output("Alec Asher bats R.\n").to_stdout
+            expect{get_bats(Player.find_by(full_name:"Alec Asher"))}.to output("Alec Asher bats R.\n").to_stdout
       end
       it "return the age for the player 'alec asher'" do
         #binding.pry
-            expect{get_age(Player.find(14988))}.to output("Alec Asher was 27.\n").to_stdout
+            expect{get_age(Player.find_by(full_name:"Alec Asher"))}.to output("Alec Asher was 27.\n").to_stdout
       end
       it "return the height for the player 'alec asher'" do
         #binding.pry
-            expect{get_height(Player.find(14988))}.to output("Alec Asher is 76 inches tall.\n").to_stdout
+            expect{get_height(Player.find_by(full_name:"Alec Asher"))}.to output("Alec Asher is 76 inches tall.\n").to_stdout
       end
       it "return the jersey number for the player 'alec asher'" do
         #binding.pry
-            expect{get_jersey_num(Player.find(14988))}.to output("Alec Asher wore the number 49.\n").to_stdout
+            expect{get_jersey_num(Player.find_by(full_name:"Alec Asher"))}.to output("Alec Asher wore the number 49.\n").to_stdout
       end
       it "return the position for the player 'alec asher'" do
         #binding.pry
-            expect{get_position(Player.find(14988))}.to output("Alec Asher primarily played P.\n").to_stdout
+            expect{get_position(Player.find_by(full_name:"Alec Asher"))}.to output("Alec Asher primarily played P.\n").to_stdout
       end
 
 end
