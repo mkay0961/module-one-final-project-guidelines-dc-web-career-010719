29a22103871c36088fdc43e8e44b064ab6c22bff
mkay0961 <mkay0961@gmail.com>
2019-01-24 22:33:49 -0500

    added stats

--- app/UI/stats.rb
+++ app/UI/stats.rb
@@ -17,41 +17,77 @@ def get_stats(id)
   pitching = JSON.parse(response.body)
   pitchinfo = pitching["sport_pitching_tm"]["queryResults"]
 #binding.pry
-  if pitchinfo.size == 3
+  case hinfo.size
+  when 3
+    hingo1 = hinfo["row"]
+    puts "\nThe hitting STATS for #{Player.find_by(playerid: id).full_name}"
+    table = Terminal::Table.new do |rows|
+      rows << ['AVG', hingo1["avg"]]
+      rows << :separator
+      rows << ['HR', hingo1["hr"]]
+      rows << :separator
+      rows << ['RBI', hingo1["rbi"]]
+      rows << :separator
+      rows << ['SB', hingo1["sb"]]
+      rows << :separator
+      rows << ['SO', hingo1["so"]]
+    end
+    puts table
+  when 2
+    puts "\nThere are no hitting STATS for #{Player.find_by(playerid: id).full_name}"
+  end
+
+
+
+  case pitchinfo.size
+  when 3
     pitchinfo1 = pitchinfo["row"]
 
-    puts "The pitching STATS for #{Player.find_by(playerid: id).full_name}"
-
-    rows = []
-    rows << ['ERA', pitchinfo1["era"]]
-    rows << ['ER', pitchinfo1["er"]]
-    rows << ['W', pitchinfo1["w"]]
-    rows << ['SO', pitchinfo1["so"]]
-    rows << ['BB', pitchinfo1["bb"]]
-    rows << ['HR', pitchinfo1["hr"]]
-    rows << ['SV', pitchinfo1["sv"]]
-    table = Terminal::Table.new :rows => rows
+    puts "\nThe pitching STATS for #{Player.find_by(playerid: id).full_name}"
+    table = Terminal::Table.new do |rows|
+      rows << ['ERA', pitchinfo1["era"]]
+      rows << :separator
+      rows << ['ER', pitchinfo1["er"]]
+      rows << :separator
+      rows << ['W', pitchinfo1["w"]]
+      rows << :separator
+      rows << ['SO', pitchinfo1["so"]]
+      rows << :separator
+      rows << ['BB', pitchinfo1["bb"]]
+      rows << :separator
+      rows << ['HR', pitchinfo1["hr"]]
+      rows << :separator
+      rows << ['SV', pitchinfo1["sv"]]
+    end
     puts table
-  end
-  if hinfo.size == 3
-    hingo1 = hinfo["row"]
-    puts "The hitting STATS for #{Player.find_by(playerid: id).full_name}"
-    rows = []
-    rows << ['AVG', hingo1["avg"]]
-    rows << ['HR', hingo1["hr"]]
-    rows << ['RBI', hingo1["rbi"]]
-    rows << ['SB', hingo1["sb"]]
-    rows << ['SO', hingo1["so"]]
-    table = Terminal::Table.new :rows => rows
-    puts table
-  end
-  if hinfo.size == 2
-    puts "There are no hitting STATS for #{Player.find_by(playerid: id).full_name}"
-  end
-  if pitchinfo.size == 2
-    puts "There are no pitching STATS for #{Player.find_by(playerid: id).full_name
+  when 2
+    puts "\nThere are no pitching STATS for #{Player.find_by(playerid: id).full_name
     }"
   end
+
+
+
+
+  prompt = TTY::Prompt.new
+  input = prompt.select("\nWhat do you want to do?", per_page: 3) do |menu|
+    menu.choice "1." + " GO BACK".red , 1
+    menu.choice "2." + " Restart ".red + "program", 2
+    menu.choice "3." + " Exit ".red + "the program", 3
+  end
+
+  case input
+  when 1
+    system "clear"
+    player_info_menu(Player.find_by(playerid: id))
+  when 2
+    system "clear"
+    get_menu
+  when 3
+    system "clear"
+    exit_program
+  end
+
+
 
 
 
